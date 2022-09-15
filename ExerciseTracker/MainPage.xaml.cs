55041efb1950f64c204e3fc0f0f989553760a461
LAPTOP-OKKSJSTR\Owner <billy.boy2000@hotmail.com>
2022-09-15 14:25:48 +0100

    Add project files.

---
+++ ExerciseTracker/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ExerciseTracker
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void UpdateEx_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new UpdateExPage());
+        }
+
+        private async void NewEx_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new NewExPage());
+        }
+
+        private async void DelEx_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new DelExPage());
+        }
+    }
+}
